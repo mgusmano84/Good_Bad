6a2c043b97100f61382a5058848d0f7f8f9ea02f
Matthew Gusmano <matthew.gusmano@gmail.com>
2016-06-21 20:04:53 -0400

    finished objective

--- http.js
+++ http.js
@@ -1,6 +1,16 @@
+var Twitter = require('twitter');
 var http = require('http');
 var PORTBad = 7000;
 var PORTGood = 7500;
+
+var twitterKeys = {
+  consumer_key: 'j815aXDKsTROutUey2UaSln7x',
+  consumer_secret: 'opSwsd6irGQj3Dn0e7iOUScNHmd8urUEi9SOyqKQPLxivJZAWs',
+  access_token_key: '580525093-D9IGytxZdL8br2feqQDtWld0lDhzUfv33RpNHlsE',
+  access_token_secret: 'oFAmw4RVJJr4YRG28aBwORdJqU8RkVpi5EO15FByYemxP',
+}
+
+
 
 //return good
 var server = http.createServer(function(req, res) {
@@ -10,9 +20,22 @@ var server = http.createServer(function(req, res) {
 		'Content-Type': 'text/html'
 	});
 
-	var bad = "<u><b>You Suck!</b></u>"
+	//if twitter needs to be used
+	var twit = function() {
+		var client = new Twitter(twitterKeys);
+		client.get('statuses/user_timeline', {screen_name: 'gusmano84', count: 20}, function(error, tweets, response) {
+		for (var prop in tweets) {
+  			console.log(tweets[prop].text);
+  		}
+  		})
+	}
 
-	res.end(bad);
+	var bad = Array("you suck","you bite","Loser","Slacker","weak");
+	var bader = bad[Math.floor(Math.random()*bad.length)];
+
+	var reallybad = bader
+
+	res.end(bader);
 });
 
 //return bad
@@ -23,9 +46,12 @@ var server2 = http.createServer(function(req, res) {
 		'Content-Type': 'text/html'
 	});
 
-	var bad = "<b>You Rock!!!!!!</b>"
+	var good = Array("you are awesome","love you","my angel","the best","you are amazing");
+	var gooder = good[Math.floor(Math.random()*good.length)];
 
-	res.end(bad);
+	var reallygood = gooder
+
+	res.end(reallygood);
 });
 
 server.listen(PORTBad, function() {
@@ -38,3 +64,4 @@ server2.listen(PORTGood, function() {
 
 
 // http://localhost:7000/
+// http://localhost:7500/
